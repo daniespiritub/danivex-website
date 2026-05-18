a3dc633ec1576f3179a7d82a837b724d79472972
Danivex <judaesbravo5@gmail.com>
2026-05-18 20:00:04 +0200

    Fix Monetag service worker zone

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11024752
+    "zoneId": 11024851
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
